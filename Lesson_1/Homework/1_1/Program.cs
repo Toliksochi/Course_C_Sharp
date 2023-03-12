12f7fd0a313621bfccf981ce931ae3b6ce8bab5c
Toliksochi <tolik_sochi@mail.ru>
2023-03-12 11:58:53 +0300

    Homework 1_1

---
+++ Lesson_1/Homework/1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+    Console.Write("max = " + b + " ; " + "min = " + a); 
+}
+else if (a > b)
+{
+    Console.Write("max = " + a + " ; " + "min = " + b); 
+}
+else
+{
+    Console.Write("Числа равны");
+} 
+
+
+
+
