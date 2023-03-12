40af1ee97dc2abf49dbdc1116d28ea5125bc95d1
Toliksochi <tolik_sochi@mail.ru>
2023-03-12 13:20:53 +0300

    Homework 1_4

---
+++ Lesson_1/Homework/1_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+
+int Z = 1;
+
+while (Z <= N)
+{
+    if (Z % 2 == 0)
+    {
+        Console.Write(Z + ", ");
+        Z = Z + 2;
+    } 
+    else
+    {
+        Z = Z +1;
+    }
+}
+
