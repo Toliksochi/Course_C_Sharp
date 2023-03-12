e7b35d3899c8fb32a0c52ef9904b670dc30beb46
Toliksochi <tolik_sochi@mail.ru>
2023-03-12 13:09:58 +0300

    Homework 1_3

---
+++ Lesson_1/Homework/1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 2 == 0)
+{
+   Console.WriteLine("Число А  - четное"); 
+}
+else
+{
+    Console.WriteLine("Число А  - нечетное"); 
+}
