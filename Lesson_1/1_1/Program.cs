c562aec07dd13c4d4d4d3126baa532efc7b58aea
Toliksochi <tolik_sochi@mail.ru>
2023-03-12 11:15:04 +0300

    Task_1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a == b*b)
+{
+    Console.WriteLine ("да");
+}
+else
+{
+    Console.WriteLine ("нет");
+};
