b98d8885c82a6044c8ff6abc179dfaf4259b97ad
Toliksochi <tolik_sochi@mail.ru>
2023-03-13 19:56:08 +0300

    homework_task_2_4

---
+++ Lesson_2/Homework/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿void Weekend(int num)
+{
+    if (num > 5 && num < 8)
+    {
+        Console.WriteLine ("Вы выбрали выходной");   
+    }
+    else if (num >= 1 && num < 6)
+    {
+        Console.WriteLine ("Вы выбрали рабочий день");
+    }
+    else
+    {
+        Console.WriteLine ("Вы ввели неверное число, попробуйте еще раз");
+    }    
+}
+Console.WriteLine ("Введите число от 1 до 7");
+Weekend(int.Parse (Console.ReadLine()!));
