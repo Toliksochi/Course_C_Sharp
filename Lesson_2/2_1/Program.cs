390c7bc584f41af2a6e5a9370835eacc62633029
Toliksochi <tolik_sochi@mail.ru>
2023-03-13 15:46:58 +0300

    Task_2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Takenum(int num)
+{
+    Console.WriteLine (num);
+    int a = num %10;
+    int b = num/10;
+    if (a>b)
+    {
+     return a;
+    }
+    else
+    {
+     return b;
+    }
+}
+
+int result = Takenum (new Random().Next (10,100));
+Console.WriteLine (result);
