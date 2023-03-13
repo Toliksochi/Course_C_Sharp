56a3bd856f6fc75fa3498fcdcfc37a7c13a94811
Toliksochi <tolik_sochi@mail.ru>
2023-03-13 16:51:35 +0300

    Task_2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+void Takenum (int num1, int num2)
+{
+    if (num1 % num2 == 0)
+        Console.WriteLine ("кратно");
+    else 
+        Console.WriteLine ($"не кратно {num1 % num2}");
+          
+}
+Takenum (16, 4);
