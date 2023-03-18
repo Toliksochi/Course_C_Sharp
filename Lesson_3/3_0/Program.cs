8acfb1edcabcb9f15dfd88e9de87efb258e29d88
Toliksochi <tolik_sochi@mail.ru>
2023-03-18 13:33:35 +0300

    task_3_0

---
+++ Lesson_3/3_0/Program.cs
@@ -0,0 +1,17 @@
+﻿void Quaters(int x, int y)
+{
+    if (x == 0 || y ==0)
+        Console.WriteLine ("x = 0, y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine ("Четверть I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine ("Четверть II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine ("Четверть III");
+    else
+        Console.WriteLine ("Четверть IV");
+}
+
+int num_1 = int.Parse (Console.ReadLine ()!);
+int num_2 = int.Parse (Console.ReadLine ()!);
+Quaters (num_1, num_2);
