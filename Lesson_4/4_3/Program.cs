81c0964459c9f28f1c2947189acbf33f91b7baa4
Toliksochi <tolik_sochi@mail.ru>
2023-03-19 22:50:00 +0300

    task_4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Massiv (int x)
+{
+    int[] arr = new int [x];
+    for (int i =0; i < x; i++)
+    {
+        arr [i] = new Random().Next(2);
+        Console.Write (arr[i]+ ", ");
+    }
+}
+
+Massiv (int.Parse (Console.ReadLine()!));
