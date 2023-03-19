acff983d3f8697a290d563409889b7d1586e2b68
Toliksochi <tolik_sochi@mail.ru>
2023-03-19 22:18:14 +0300

    task_4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumNum (int num)
+{
+    int kol =0;
+    for (int i = 1; num > 0; i++)
+    {
+        num = num/10;
+        kol =i;
+    }
+    return kol;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
